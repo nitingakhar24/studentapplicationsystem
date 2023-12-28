ae759d3507257dbc7369ce3b919c91f3d6057bfe
nitingakhar <nitingakhar@yahoo.com>
2023-12-27 21:08:31 -0500

    Backend changes with RestController, services for Student Application

---
+++ src/main/java/com/student/application/controller/StudentController.java
@@ -0,0 +1,42 @@
+package com.student.application.controller;
+
+import com.student.application.model.Student;
+import com.student.application.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+@RequiredArgsConstructor
+public class StudentController {
+    private final StudentService studentService;
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getStudents() {
+        return new ResponseEntity<>(studentService.getStudents(), HttpStatus.FOUND);
+    }
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student) {
+        return studentService.addStudent(student);
+    }
+
+    @PutMapping("/update/{id}")
+    public Student updateStudent(@RequestBody Student student, @PathVariable Long id) {
+        return studentService.updateStudent(student, id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+    }
+
+    @GetMapping("/student/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
+    }
+}
--- src/main/java/com/student/application/service/impl/StudentServiceImpl.java
+++ src/main/java/com/student/application/service/impl/StudentServiceImpl.java
@@ -1,33 +1,61 @@
 package com.student.application.service.impl;
 
+import com.student.application.exception.StudentAlreadyExistException;
+import com.student.application.exception.StudentNotFoundException;
 import com.student.application.model.Student;
+import com.student.application.repository.StudentRepository;
 import com.student.application.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
     @Override
     public Student addStudent(Student student) {
-        return null;
+        if (studentAlreadyExists(student.getEmail())) {
+            throw new StudentAlreadyExistException(student.getEmail() + "already exists !");
+        }
+        return studentRepository.save(student);
     }
+
+
 
     @Override
     public List<Student> getStudents() {
-        return null;
+        return studentRepository.findAll();
     }
 
     @Override
     public Student updateStudent(Student student, Long id) {
-        return null;
+        return studentRepository.findById(id).map(st -> {
+            st.setFirstName(student.getFirstName());
+            st.setLastName(student.getLastName());
+            st.setDepartment(student.getDepartment());
+            st.setEmail(student.getEmail());
+            return studentRepository.save(st);
+        }).orElseThrow(() -> new StudentNotFoundException("Sorry, this Student could not be found"));
     }
 
     @Override
     public Student getStudentById(Long id) {
-        return null;
+        return studentRepository.findById(id)
+                .orElseThrow(() -> new StudentNotFoundException("Sorry, no student found with the Id : " + id));
     }
 
     @Override
     public void deleteStudent(Long id) {
+        if (!studentRepository.existsById(id)) {
+            throw new StudentNotFoundException("Sorry, student not found");
+        }
+        studentRepository.deleteById(id);
+    }
 
+    private boolean studentAlreadyExists(String email) {
+        return studentRepository.findByEmail(email).isPresent();
     }
 }
